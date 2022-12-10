f8541346960ca275dc0cfbef8c79c74a518db5e0
Olamilekan <nunuolamilekan@gmail.com>
2022-12-10 16:20:55 +0100

    change http to https

--- utils/fetchData.ts
+++ utils/fetchData.ts
@@ -1,6 +1,6 @@
 const fetchData = async (searchValue: string) => {
-  const api_key = process.env.API_KEY
-  const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=${api_key}&m=genre`;
+  const api_key = process.env.API_KEY;
+  const url = `https://www.omdbapi.com/?s=${searchValue}&apikey=${api_key}&m=genre`;
   const res = await fetch(url);
   const resJson = await res.json();
 
